07532b1cdf566e897078b666e3b454a3b0e647a4
Jeffry Gonzalez <Jeff@hypertheory.com>
2022-03-15 14:14:12 -0400

    created some less but still fake services

--- CampsiteReservationsApi/Program.cs
+++ CampsiteReservationsApi/Program.cs
@@ -1,7 +1,11 @@
+global using CampsiteReservationsApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
+builder.Services.AddTransient<ICheckTheStatus, LocalStatusService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
