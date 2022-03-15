6092e203f1281d4d920041cb623f5e66e60ba8c1
Jeffry Gonzalez <Jeff@hypertheory.com>
2022-03-15 12:30:53 -0400

    Add project files.

---
+++ CampsiteReservationsApi/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampsiteReservationsApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public async Task<IActionResult> GetTheStatus()
+    {
+        return Ok(new GetStatusResponse("Looks Good", "Bob", DateTime.Now));
+    }
+}
+
+public record GetStatusResponse(string status, string oncall, DateTime whenChecked);
